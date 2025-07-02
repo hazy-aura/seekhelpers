dbf6d6d61a40a8aaef73570536297f8bda4da9d7
Ayush Rana <108958206+hazy-aura@users.noreply.github.com>
2025-07-02 13:51:39 +0530

    changed backend port

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/api', taskRoutes);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 connectDB();
 app.listen(port, ()=>{
